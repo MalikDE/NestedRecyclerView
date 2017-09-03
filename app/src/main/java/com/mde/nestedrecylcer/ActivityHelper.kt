774d56ea98d93cbdc8ee86904260acb71e7da660
Malik <malik.delpech+git@gmail.com>
2017-09-03 12:31:59 +0200

    clean code

--- app/src/main/java/com/mde/nestedrecylcer/ActivityHelper.kt
+++
@@ -1,9 +0,0 @@
-package com.mde.nestedrecylcer
-
-import android.app.Activity
-import android.support.annotation.IdRes
-import android.view.View
-
-fun <T : View> Activity.bind(@IdRes res: Int): Lazy<T> {
-    return lazy { findViewById<T>(res) }
-}
